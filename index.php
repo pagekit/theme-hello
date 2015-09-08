f8594256fb159b30835b3f7946e8b92854ba8ddf
Florian <florian@yootheme.com>
2015-09-08 14:59:54 +0200

    Added detailed explanation to the module definition

--- index.php
+++ index.php
@@ -5,12 +5,20 @@
  */
 return [
 
+    /**
+     * Unique name that identifies your theme.
+     */
     'name' => 'hello-theme',
 
+    /**
+     * Define the type of this module definition.
+     * Makes sure Pagekit loads and recognizes this package as a theme.
+     */
     'type' => 'theme',
 
     /**
-     * Define resources
+     * Register shorthands. Used when referencing resources.
+     * Example: theme:image.jpg -> packages/pagekit/theme-hello/image.jpg
      */
     'resources' => [
 
@@ -20,7 +28,9 @@ return [
     ],
 
     /**
-     * Define menu positions
+     * Define menu positions.
+     * Will be listed in the backend so that users
+     * can assign menus to these positions.
      */
     'menus' => [
 
@@ -29,7 +39,9 @@ return [
     ],
 
     /**
-     * Define widget positions
+     * Define widget positions.
+     * will be listed in the backend so that users
+     * can publish widgets in these positions.
      */
     'positions' => [
 
@@ -38,7 +50,10 @@ return [
     ],
 
     /**
-     * Define settings
+     * Define theme configuration.
+     * This is the theme's default configuration. During run-time, they will be merged with
+     * settings the user has set in the backend. The default configuration can therefore
+     * be overwritten.
      */
     'config' => []
 
